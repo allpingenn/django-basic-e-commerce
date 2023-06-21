b0ab1ae5ecf4ea3a03edef9768af499c27e42de0
alpingen <alptugnamigur@gmail.com>
2023-06-22 01:25:48 +0300

    messages added

--- user/views.py
+++ user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import *
 from django.contrib.auth import authenticate, login, logout
 from products.models import Customer
+from django.contrib import messages
 
 def userRegister(request):
     form = UserForm()
@@ -11,13 +12,13 @@ def userRegister(request):
             user = form.save()
             customer = Customer(user=user)
             customer.save()
+            messages.success(request,"Registration successful. You can now log in.")
             return redirect('login')
         
     context = {
         'form': form,
     }
     return render(request, 'register.html', context)
-
 
 def userLogin(request):
     if request.method == 'POST':
@@ -28,13 +29,17 @@ def userLogin(request):
         
         if user is not None:
             login(request, user)
+            messages.success(request, 'Login Successfully')
             return redirect('index')
         else:
             logout(request)
+            messages.error(request, 'Invalid Username or Password')
             return redirect('login')
     
     return render(request, 'login.html')
 
+
 def userLogout(request):
     logout(request)
+    messages.success(request, 'Successfully Logged Out')
     return redirect('index')
